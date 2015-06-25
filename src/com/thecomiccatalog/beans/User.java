ccef6845db997e3a41b68e770bc533db92bb9dad
Andres <andreshw@gmail.com>
2015-06-24 22:39:04 -0500

    se agrgan algunas clases

--- src/com/thecomiccatalog/beans/User.java
+++ src/com/thecomiccatalog/beans/User.java
@@ -1,5 +1,23 @@
 package com.thecomiccatalog.beans;
 
 public class User {
+	private int idUser;
+	
+	private String userName;
 
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 }
---
+++ src/com/thecomiccatalog/dao/SecurityDAO.java
@@ -0,0 +1,5 @@
+package com.thecomiccatalog.dao;
+
+public class SecurityDAO {
+
+}
